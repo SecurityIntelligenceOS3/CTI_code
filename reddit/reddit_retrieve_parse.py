9081fee2be3945edd1ae4e0e78e00347d31b69b1
Diana Rusu <russu_diana@yahoo.com>
2015-01-20 13:18:48 +0100

    small changes to analyse and reddit

--- reddit/reddit_retrieve_parse.py
+++ reddit/reddit_retrieve_parse.py
@@ -13,8 +13,9 @@ collection.remove({})
 #reddit api gives a lot of options in retrieving information you want
 #for ex the new 100 posts : http://www.reddit.com/r/blackhat/new.json?sort=new&limit=100
 #or if you want directly get the posts in json format, simply /.json
+#for ex http://www.reddit.com/r/blackhat/.json?limit=50
 # Limit : default: 25, maximum: 100
-response = urllib2.urlopen('http://www.reddit.com/r/blackhat/.json?limit=50')
+response = urllib2.urlopen('http://www.reddit.com/r/blackhat/new.json?sort=new&limit=100')
 
 #retrieve the json data
 data = json.loads(response.read())
@@ -29,6 +30,11 @@ for i in data["data"]["children"]:
 	entry={"url":url,"title":title,"score":score,"num_comments":num_comments,"ups":ups}
 	#insert the entry in mongoDB
 	collection.insert(entry)
+
+#Remove duplicates from MongoDB :
+collection.ensureIndex( { url:1,paste:1}, { unique:true, dropDups:true } )
+#delete all empty documents ( if title is empty then there is no message to process ) 
+collection.remove( { "title" : [] } )
 
 print "-------------Check for data in mongoDB-------------------"
 
